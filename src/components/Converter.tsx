4e8076413f1235541c45e527d6f51e985cd7b55a
Enzo Giacomelli <egiacomelli09@gmail.com>
2026-04-13 16:22:41 -0300

    criação do select de moeda destino, exibição do resultado com clique no botão

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { getRates } from "../services/currencyService";
+import { resume } from "react-dom/server";
 
 export default function Converter() {
     const [amount, setAmount] = useState(1);
     const [fromCurrency, setFromCurrency] = useState("USD");
-    const [toCurrency, setToCurrency] = useState("EUR");
+    const [toCurrency, setToCurrency] = useState("BRL");
     const [convertedAmount, setConvertedAmount] = useState<number | null>(null);
 
-    const currencies = ["USD", "EUR", "GBP", "JPY", "AUD"];
+    const currencies = ["BRL", "USD", "EUR", "GBP", "JPY", "AUD"];
 
     const convert = async () => {
         try {
@@ -36,9 +37,19 @@ export default function Converter() {
                 ))}
             </select>
 
+            <select value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
+                {currencies.map((c) => (
+                    <option key={c} value={c}>
+                        {c}
+                    </option>
+                ))}
+            </select>
+            
+            <button onClick={convert}>Converter</button>
+            
+            <p>Resultado: {convertedAmount !== null ? convertedAmount.toFixed(2) : "0.00"}</p>
 
         </div>
-
     );
 }
 
@@ -84,19 +95,27 @@ criando o return do JSX:
         essa função pode ser criada separadamente ou diretamente no onChange, como foi feito aqui
     
     criando o select de moeda base:
-        entender e documentar o select
+        o valor do select é controlado pela variável fromCurrency
+        quando o evento onChange ocorre, atualiza o estado fromCurrency com o valor selecionado com e.target.value
+        para listar as opções do select, o metodo map é chamado para criar um option para cada item do array currencies
+        cada option tem uma key unica (nesse caso, o proprio valor da moeda) e o valor do option é a moeda (c)
+        a moeda é exibida dentro do option com o mesmo valor do option
 
 
     criando o select de moeda destino:
-        criar o select se baseando no anterior
-
+        igual o select anterior mas controlado pela variavel toCurrency
+        
 
     criando o botão de converter:
-        botão devera chamar a funcão OnClick q deve receber a funcao convert
+        botão com o texto "Converter"
+        no evento de onClick, chama a função convert para realizar a conversão
+        a função convert atualiza o valor de convertedAmount
 
 
     exibir resultado:
-        
+        um paragrafo que exibe o resultado da conversão
+        se convertedAmount for diferente de null, exibe o valor formatado com 2 casas decimais usando toFixed(2)
+        caso contrário, exibe "0.00" como valor padrão
 
     fim da div
 
